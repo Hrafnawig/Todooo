d53f408aaec1e346dd39c2af89638cd879ef0d1e
Alex Kobyakovski <74356061+Hrafnawig@users.noreply.github.com>
2021-03-10 12:10:53 +0200

    Todoo

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,27 @@
+import React ,{ useState } from 'react'
+import TodoHeader from '../Components/TodoHeader';
+import TodoList from '../Components/TodoList';
+import TodoForm from '../Components/TodoForm';
+export default function Home() {
+    const [tasks,setTasks]= useState([{
+        id:Date.now(),
+        isDone:false,
+        body: '',
+    }])
+    const addTask=values=>{
+        const newTask={
+         body:values.addingTask,
+         id:Date.now(),
+         isDone:false,
+        }
+        setTasks([...tasks,newTask]);
+    }
+    const deleting=(id)=>{setTasks(tasks.filter(currentTask=>currentTask.id!==id))}
+    return (
+        <div>
+            <TodoHeader/>
+            <TodoList tasks={tasks} Delete={deleting}/>
+            <TodoForm addTask={addTask} tasks={tasks}/>
+        </div>
+    )
+}
